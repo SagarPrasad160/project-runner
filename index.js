2bc67cb57161882c1d75b55fe3ac67a75e95cc13
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-17 20:17:00 +0530

    Add spawn method from child_process

--- index.js
+++ index.js
@@ -2,11 +2,32 @@
 
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
 
-const start = debounce(() => {
-  console.log("Started Program");
-}, 100);
+program
+  .version("1.0.0")
+  .argument("[filename]", "Name of file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+    try {
+      await fs.promises.access(name);
+    } catch (error) {
+      throw new Error(`${name} file not found`);
+    }
 
-chokidar.watch(".").on("add", start).on("change", start).on("unlink", start);
+    const start = debounce(() => {
+      spawn("node", [name], { stdio: "inherit" });
+    }, 100);
 
-console.log("hi changed");
+    chokidar
+      .watch(".", {
+        ignored: /(\.git)|(node_modules)/,
+      })
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
